2cbfa0664b9b7e716fd99c17ccd6cea37a1939f3
bezalel6 <bezalel3250@gmail.com>
2026-01-02 09:12:27 +0200

    Add GitHub Pages deployment workflow and update configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const repoName = 'rst-israel'; // Change if your repo name is different
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
+  // Set these for GitHub Pages
+  ...(isGithubPages && {
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
+  })
 };
 
 export default nextConfig;
